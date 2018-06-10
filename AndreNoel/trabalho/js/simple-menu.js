f70ee5462cf6764a948b920cd8b9d0241f3a7e82
Diogo Vanderlan <diogo.vanderlan@gmail.com>
2018-06-10 01:40:42 -0300

    Trabalho de JQuery com plugins

---
+++ AndreNoel/trabalho/js/simple-menu.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $('ul.parent > li').hover(
+        function(){
+            $(this).find('ul.child').show(200);
+        
+        }, function() {
+            $(this).find('ul.child').hide(200);
+
+    });
+});
